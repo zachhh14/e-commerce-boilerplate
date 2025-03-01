19eb84317202bac4301aaf3ab30772920fdea8a9
zachhh14 <zactec115@gmail.com>
2025-03-01 20:55:11 +0800

    remove the middleware for the meantime

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -2,6 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
+    return NextResponse.next()
     let supabaseResponse = NextResponse.next({
         request,
     })
